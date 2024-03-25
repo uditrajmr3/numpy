55cc080bd7ab3a7dcd857f9ad1526854f040a85e
udit raj <uditraj3426@gmail.com>
2024-03-26 00:51:29 +0530

    Array Iteration

--- 03_array_indexing.py
+++ 03_array_indexing.py
@@ -7,7 +7,7 @@ _2d_array = np.array([[1, 2], [3, 4]])
 print(_2d_array[0][1])  # second element for first row or row1 column2
 print(_2d_array[0, 1])
 
-_3d_array = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]], [[13, 14, 15], [16, 7, 18]]])
+_3d_array = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]], [[13, 14, 15], [16, 17, 18]]])
 print(_3d_array[1][1][0])  # first element of the second column of second row or row2 column2 element1
 print(_3d_array[1, 1, 0])
 
