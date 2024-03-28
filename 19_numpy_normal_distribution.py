5a73703f2ed91971c336f78e47627afc3f266a72
udit raj <uditraj3426@gmail.com>
2024-03-29 02:58:12 +0530

    Normal Distribution and its Data Visualization

---
+++ 19_numpy_normal_distribution.py
@@ -0,0 +1,19 @@
+# !!! Gaussian Distribution (Normal Distribution)
+# ! note - dist. : distribution
+# loc: mean, scale: standard deviation, size: size
+
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# normal dist. of size 2,3
+arr = random.normal(size=[2, 3])
+print(arr)
+
+# normal dist. of size 2,3 with mean 1 and sd 2
+new_arr = random.normal(loc=1, scale=2, size=(2, 3))
+print(new_arr)
+
+# visualization of normal dist. (bell curve)
+sns.displot(random.normal(size=1000))
+plt.show()
