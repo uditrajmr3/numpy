ae1296ca16d4eecdebf1bb57bb9df2a37f0a13cb
udit raj <uditraj3426@gmail.com>
2024-04-05 01:35:25 +0530

    Numpy Difference

---
+++ 36_numpy_difference.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+arr1 = np.array([1, 2, 3, 4, 5])
+arr2 = np.array([6, 7, 8, 9, 10])
+
+# discrete difference of 2 arrays
+difference_dis_arr1 = np.diff(arr1)
+print(difference_dis_arr1)
